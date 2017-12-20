529252e30c011780312280dda57dd3912d800e8c
Tom Ka Chun Cheung <tomhk1123@gmail.com>
2017-12-20 15:12:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import IdeasContainer from './components/IdeasContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Idea Board</h1>
+        </header>
+        <IdeasContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/IdeasContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class IdeasContainer extends Component {
+  state = {
+    ideas: []
+  }
+
+  componentDidMount() {
+      
+  }
+
+  render() {
+    return (
+      <div className="">
+        Ideas
+      </div>
+    );
+  }
+}
+
+export default IdeasContainer;
